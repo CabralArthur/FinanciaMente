af55dac05ffbfad22277b628395410476624f0cb
Ah-Cabral <63428806+Ah-Cabral@users.noreply.github.com>
2021-03-30 22:53:52 -0300

    Fazendo Script para aparecer o Modal

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,4 +1,18 @@
-VanillaTilt.init(document.querySelector(".card-about"), {
-    max: 20,
-    speed: 300
+VanillaTilt.init(document.querySelector('.card-about'), {
+  max: 20,
+  speed: 300,
 });
+
+function Fecha(id) {
+  let modalBackground = document.getElementById('modalbg');
+  tipoModal = document.getElementById(id);
+  tipoModal.style.display = 'none';
+  modalBackground.style.display = 'none';
+}
+
+function Abre(id) {
+  let modalBackground = document.getElementById('modalbg');
+  tipoModal = document.getElementById(id);
+  tipoModal.style.display = 'inline';
+  modalBackground.style.display = 'inline';
+}
